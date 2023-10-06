35303446826eee4cf4edc518ad0020c7c0e59154
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-10-07 03:55:06 +0800

    wew

--- src/pages/File.jsx
+++ src/pages/File.jsx
@@ -5,53 +5,60 @@ import toast from 'react-hot-toast';
 export default function File({ supabaseClient }) {
 
 	const [ image, setImage ] = useState(null)
+	const [imageList, setImageList] = useState([]);
 	const [ imageUrl, setImageUrl ] = useState('')
 
 	const uploadFile = async() => {
 		if (image) {
 
-			const { data, error } = await supabaseClient.storage
-			.from('images') // Replace with your bucket name
-			.upload('sampleFile', image); // Pass the headers with the JWT token
+			console.log(image)
 
+			const { data, error } = await supabaseClient.storage
+				.from('images')
+				.upload(image.name, image)
 		
 			if (error) {
 				toast.error(error)
-				console.error('Error uploading file:', error);
 			} else {
 				toast.success('File uploaded successfully')
-				console.log('File uploaded successfully:', data);
 			}
+
 		}
 	}
 
 	const fetchImageURL = async () => {
-		const imageFileName = 'sampleFile'; // Replace with the actual image file name
 		const { data, error } = await supabaseClient.storage
-		.from('images')
-		.getPublicUrl(`${imageFileName}`);
+			.from('images')
+			.list()
 	
 		if (error) {
-			console.error('Error fetching image URL:', error.message);
+			console.error('Error fetching image URL:', error.message)
 		} else {
-			setImageUrl(data.publicUrl)
-			console.log('Image URL:', data.publicUrl);
+			const filteredImages = data.filter((file) => file.name !== '.emptyFolderPlaceholder');
+
+			const images = filteredImages.map((file) => ({
+				name: file.name,
+				url: supabaseClient.storage.from('images').getPublicUrl(`${file.name}`) // Construct the URL
+			}));
+			setImageList(images)
+			console.log(images)
 		}
-		
 	};
 
 	return (
-		<div className='mx-3 lg:mx-0'>
+		<div className='mx-3 lg:mx-auto'>
 			<input onChange={ (e)=>setImage( e.target.files[0]) } type="file" className="file-input file-input-bordered w-full my-2" />
 			<button className='btn btn-success w-full my-2' onClick={ uploadFile }>Upload</button>
 			<button className='btn btn-primary w-full my-2' onClick={ fetchImageURL }>Fetch</button>
 
-			{!imageUrl==''?(
-				<ul>
-					<li>
-						<img src={ imageUrl } alt="" />
-					</li>
-				</ul>
+			{imageList?(
+				<div className='grid grid-cols-2 gap-2'>
+					{imageList.map((image, index) => (
+						<div key={index} className='object-contain mask-square'>
+							<img className='h-full rounded-md' src={image.url.data.publicUrl} alt={`Image ${index}`} />
+						</div>
+					))}
+				</div>
 			):null}
 			
 		</div>
